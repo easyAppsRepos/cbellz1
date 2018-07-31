efe85fdc6048ccf5caf1b2483200a92d65182954
easyAppsRepos <jralfarog@gmail.com>
2018-07-30 22:37:26 -0600

    arreglos1

--- build/25.js
+++ build/25.js
@@ -72,7 +72,7 @@ var AyudaPage = (function () {
         console.log('ionViewDidLoad AyudaPage');
         this.information = [{ nombre: 'Reservas y Citas', id: 1, respuesta: 'Puedes ver el estado de tus reservaciones en la pestaña Reservaciones', open: false },
             { nombre: 'Uso de cupones', id: 2, respuesta: 'Para hacer uso de nuestros cupones solo debes ingresar uno y el descuento se aplicará automaticamente en tu proxima reserva', open: false },
-            { nombre: 'Como puedo formar parte de BeYou', id: 3, respuesta: 'Para formar parte de BeYou negocios debes contactarnos al email email@contacto.com o por medio del telefono 39393939', open: false }];
+            { nombre: 'Como puedo formar parte de YOURBEAUTY', id: 3, respuesta: 'Para formar parte de YOURBEAUTY negocios debes contactarnos al email email@contacto.com o por medio del telefono 39393939', open: false }];
         //this.fechaSeleccionada = new Date(Date.now());
     };
     AyudaPage.prototype.toggleSection = function (i) {
@@ -88,9 +88,10 @@ var AyudaPage = (function () {
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
             selector: 'page-ayuda',template:/*ion-inline-start:"/Users/jose/Documents/beyouApp/beYou/src/pages/ayuda/ayuda.html"*/'<!--\n  Generated template for the AjustesPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n\n   <ion-buttons start>\n    <button ion-button menuToggle>\n      <ion-icon name="menu"></ion-icon>\n    </button>\n    </ion-buttons>\n\n    \n    <ion-title>Ayuda</ion-title>\n\n\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n\n<ion-content style=\'background-color: #fafafa; \' >\n\n\n\n		<ion-list class="accordion-list">\n			<ion-list-header  [ngClass]="{\'borderSelected\': item.open, \'borderSelected2\': item.open && item.id==1}" *ngFor="let item of information; let i = index" no-lines no-padding>\n\n\n			<button style=\'        white-space: normal !important;\n    color: #777;background: transparent !important;\n    border-bottom: solid 1px #d3d3d352\'  ion-item (click)="toggleSection(i)" detail-none [ngClass]="{\'section-active\': item.open, \'section\': !item.open}">\n			<ion-icon class=\'btnList\' item-right  name="ios-add" *ngIf="!item.open"></ion-icon>\n			<ion-icon class=\'btnList\' item-right name="ios-remove" *ngIf="item.open"></ion-icon>\n\n			{{ item.nombre }}\n\n			</button>\n\n			<ion-list class=\'faqRespuesta\' *ngIf="item.open" no-lines >\n			{{item.respuesta}}\n			</ion-list>\n\n			</ion-list-header>\n		</ion-list>\n\n\n\n</ion-content>\n'/*ion-inline-end:"/Users/jose/Documents/beyouApp/beYou/src/pages/ayuda/ayuda.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["NavController"], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["NavParams"]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["NavController"] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["NavController"]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["NavParams"] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["NavParams"]) === "function" && _b || Object])
     ], AyudaPage);
     return AyudaPage;
+    var _a, _b;
 }());
 
 //# sourceMappingURL=ayuda.js.map
